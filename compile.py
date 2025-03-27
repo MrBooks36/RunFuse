3e66b6782740db918da8f347e0dd231903e3789e
MrBooks36 <mrbooks36@outlook.com>
2025-03-27 12:57:18 +1000

    Update compile.py

--- compile.py
+++ compile.py
@@ -1,19 +1,5 @@
 def make_mrb36(argv):
-    from PyInstaller.__main__ import run
-    from os import system, getcwd, path
-    from shutil import rmtree
-    name = argv[2].replace('.py', "")
-    if path.exists(name):
-     rmtree(name)
-
-    if not path.exists(name+'.spec'):
-     run([name+'.py','--noconfirm'])
-
-    run([name+'.spec','--noconfirm'])
-
-    system(f'move "{getcwd()}\\dist\\{name}"  {name}')
-    rmtree(f'{getcwd()}\\dist')
-    rmtree(f'{getcwd()}\\build')
-
-    system(f'tar -rf {name}.mrb36 {name}')
-    rmtree(name)
+    from os import system
+    from os.path import basename, splitext
+    name = basename(argv[2])
+    system(f'tar -zf {name}.mrb36 {argv[2]}')
