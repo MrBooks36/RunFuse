2742ba54da39486e03d7d4c5d3dd2fce310ba63a
MrBooks36 <mrbooks36@outlook.com>
2025-03-27 13:14:22 +1000

    Update compile.py

--- compile.py
+++ compile.py
@@ -2,4 +2,4 @@ def make_mrb36(argv):
     from os import system
     from os.path import basename, splitext
     name = basename(argv[2])
-    system(f'tar -zf {name}.mrb36 {argv[2]}')
+    system(f'tar -czf {name}.mrb36 {argv[2]}')
