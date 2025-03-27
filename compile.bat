203e4465a83db461f175be367fb6da7260a1ee16
MrBooks36 <mrbooks36@outlook.com>
2025-03-27 11:38:25 +1000

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,3 +1,2 @@
-C:\Users\%username%\AppData\Local\Programs\Python\Python312\python.exe -m nuitka --onefile --standalone --remove-output --mingw64  main.py
-del nuitka-crash-report.xml
-rename main.exe Packager.exe
+@echo off
+C:\Users\%username%\AppData\Local\Programs\Python\Python312\python.exe -m nuitka --product-name=Packager --onefile --standalone --remove-output --mingw64  main.py
