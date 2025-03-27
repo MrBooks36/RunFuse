0200cd8be8895d74edc2b88fb1e3959a01f467cd
MrBooks36 <mrbooks36@outlook.com>
2025-03-27 12:57:15 +1000

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,2 +1,4 @@
 @echo off
-C:\Users\%username%\AppData\Local\Programs\Python\Python312\python.exe -m nuitka --product-name=Packager --onefile --standalone --remove-output --mingw64  main.py
+del Packager.exe
+C:\Users\%username%\AppData\Local\Programs\Python\Python312\python.exe -m nuitka --onefile --standalone --remove-output --mingw64  main.py
+rename main.exe Packager.exe
