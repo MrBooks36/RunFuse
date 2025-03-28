2f6159138994b2c388b29639b88589ea8f007cbb
MrBooks36 <mrbooks36@outlook.com>
2025-03-28 14:24:55 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from sys import argv
 from compile import make_mrb36, prep, wrap
 from decompile import decompile
-
-if len(argv) > 1:
- if len(argv) > 2:
-  if argv[1] == 'compile': make_mrb36(argv)
-  elif argv[1] == 'wrap': wrap()
+try:
+ if len(argv) > 1:
+  if len(argv) > 2:
+   if argv[1] == 'compile': make_mrb36(argv)
+   elif argv[1] == 'wrap': wrap(argv)
+   else: decompile(argv)
+  elif argv[1] == 'prep': prep()
   else: decompile(argv)
- elif argv[1] == 'prep': prep()
- else: decompile(argv)
+except KeyboardInterrupt:
+ exit()
