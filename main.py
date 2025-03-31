685f610c3c7770ad3adc841a2cbefcdce71e503a
MrBooks36 <mrbooks36@outlook.com>
2025-03-31 17:21:31 +1000

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ if not system() == 'Windows':
 from sys import argv
 from compile import compile, wrap
 from decompile import decompile
+def uninstall():
+    import os
+    current_script_path = os.path.abspath(__file__)
+    print('Delete This Folder:'+os.path.dirname(current_script_path))
 try:
  if len(argv) > 1:
   if len(argv) > 2:
    if argv[1] == 'compile': compile(argv)
    elif argv[1] == 'wrap': wrap(argv)
    else: decompile(argv)
+  elif argv[1] == 'uninstall': uninstall()
   else: decompile(argv)
 except KeyboardInterrupt:
  exit()
