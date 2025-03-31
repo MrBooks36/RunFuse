6bdbe328a66ec39ad9f0921468d586b741383b93
MrBooks36 <mrbooks36@outlook.com>
2025-03-31 10:24:15 +1000

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,5 +1,4 @@
 @echo off
-del Packager.exe
-C:\Users\%username%\AppData\Local\Programs\Python\Python312\python.exe -m nuitka --follow-imports --standalone --remove-output --mingw64  main.py
+python -m nuitka --follow-imports --standalone --remove-output --mingw64  main.py
 rename "main.dist"/main.exe Packager.exe
 rename main.dist Packager
