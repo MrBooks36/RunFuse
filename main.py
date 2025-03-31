7df94aa838265df5e86b2813883b4dccba0b39f3
MrBooks36 <mrbooks36@outlook.com>
2025-03-31 10:24:26 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from sys import argv
-from compile import make_mrb36, prep, wrap
+from compile import compile, wrap
 from decompile import decompile
 try:
  if len(argv) > 1:
   if len(argv) > 2:
-   if argv[1] == 'compile': make_mrb36(argv)
+   if argv[1] == 'compile': compile(argv)
    elif argv[1] == 'wrap': wrap(argv)
    else: decompile(argv)
-  elif argv[1] == 'prep': prep()
   else: decompile(argv)
 except KeyboardInterrupt:
  exit()
