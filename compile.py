b86e471ee375f9d88044180067a867872f4f17fe
MrBooks36 <mrbooks36@outlook.com>
2025-04-02 10:41:20 +1000

    Update compile.py

--- compile.py
+++ compile.py
@@ -3,9 +3,11 @@ def compile(argv):
     from os import getlogin, system
     if which('Python') == None:
         print('Python is not installed or is not on path')
+        input('Press enter to exit')
         return
     if which('Python') == f'C:\\Users\\{getlogin()}\\AppData\\Local\\Microsoft\\WindowsApps\\python.exe':
-        print('Not compatible With the Microsoft Store Python version. Please install Python from the official website')
+        print('Compile is not compatible With the Microsoft Store Python version. Please install Python from the official website to use compile')
+        input('Press enter to exit')
     if not which('PyInstaller') == None: PyIstaller = True
     else: PyIstaller = False
     if not which('nuitka') == None: nuitka = True
@@ -20,6 +22,7 @@ def compile(argv):
     if pram.lower() == 'nuitka': system(f'python -m nuitka --follow-imports --standalone --remove-output --mingw64  {argv[2]}')
     elif pram.lower() == 'pyinstaller': system(f'python -m PyInstaller --onedir {argv[2]}')
     elif pram.lower() == 'py2exe': system(f'python -m py2exe -d {argv[2]}')
+    else: print(f'Invalid Input: {pram}')
 
     
 def wrap(argv):
@@ -33,11 +36,13 @@ def wrap(argv):
 
     if not dir_path.is_dir():
         print(f"Error: {argv[2]} is not a valid directory.")
+        input('Press enter to exit')
         return
 
     runtime_path = dir_path / 'runtime.json'
     if not runtime_path.exists():
         print(f"Error: runtime.json not found in {dir_path}.")
+        input('Press enter to exit')
         return
 
     try:
@@ -46,3 +51,4 @@ def wrap(argv):
         print(f"{tar_file_path} created successfully.")
     except Exception as e:
         print(f"Error: Unable to create mrb36 file {tar_file_path}: {e}")
+        input('Press enter to exit')
