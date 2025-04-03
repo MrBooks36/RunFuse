276b370d4b879b57878c4bc0d7da4fc6e0f03e6a
MrBooks36 <mrbooks36@outlook.com>
2025-04-04 09:24:38 +1000

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,3 +2,31 @@ def clean():
     from shutil import rmtree
     from os import getlogin
     rmtree(f'C:/Users/{getlogin()}/AppData/Local/Packager')
+
+def rmold():
+    import os
+    import shutil
+    import time
+
+    current_time = time.time()
+    duration_seconds = 30 * 86400  # Convert days to seconds
+
+    for folder_name in os.listdir(f'C:/Users/{os.getlogin()}/AppData/Local/Packager'):
+        folder_path = os.path.join(f'C:/Users/{os.getlogin()}/AppData/Local/Packager', folder_name)
+
+        if os.path.isdir(folder_path):
+            folder_time = os.path.getmtime(folder_path)
+            folder_age = current_time - folder_time
+
+            if folder_age > duration_seconds:
+                try:
+                    shutil.rmtree(folder_path)
+                    print(f"Deleted folder: {folder_path}")
+                except Exception as e:
+                    print(f"Error deleting folder {folder_path}: {e}")
+
+
+
+
+#setup 
+rmold()
