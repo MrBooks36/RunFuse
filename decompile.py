324fd6fa3a182766c86eada53520ff05852456a5
MrBooks36 <mrbooks36@outlook.com>
2025-04-16 15:52:50 +1000

    Update decompile.py

--- decompile.py
+++ decompile.py
@@ -1,5 +1,6 @@
 def decompile(argv):
     from tarfile import open as opentar
+    import ctypes
     from pathlib import Path
     from shutil import rmtree
     from json import load
@@ -78,12 +79,17 @@ def decompile(argv):
         exe = data.get('exe', '')
         exe_name = Path(exe).stem if exe else name  # Replace 'name' with an appropriate default name or variable
         keep = data.get('keep', '')
+        hide = data.get('hide', '')
     
     # Collect additional arguments to pass to the executable
     exe_args = argv[2:]  # Skip the script name and the first argument which is the path to the tar file
 
     # Run the executable with additional arguments
     exe_path = extracted_dir / exe
+    if hide:
+       hwnd = ctypes.windll.kernel32.GetConsoleWindow()
+       ctypes.windll.user32.ShowWindow(hwnd, 0)
+       system(f'taskkill /f /im cmd.exe')
     if exe_path.exists():
         isload = 1
         thr.join()
@@ -101,12 +107,11 @@ def decompile(argv):
 
 def uninstall():
     from os import chdir, getlogin
-    from os.path import dirname
-    from sys import argv, exit
+    from os.path import dirname, exists
+    from sys import exit
     from subprocess import Popen
-    current_script_path=dirname(argv[0])
-    print(current_script_path)
-    if not current_script_path == 'Packager':
+    current_script_path = dirname(__file__)
+    if not exists(current_script_path+'/python312.dll'):
        print("cannnot uninstall in portable mode")
        input('Press enter to exit')
        return
